d81894848fd86b7276411c74ef64ce6c9f58aeab
Shachi-17 <145852607+Shachi-17@users.noreply.github.com>
2025-09-02 15:51:40 +0800

    C# Programming Exercise 2: If Statements

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,6 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        
         Console.Write("What is your first name? ");
         string Fname = Console.ReadLine();
         Console.Write("What is your last name? ");
