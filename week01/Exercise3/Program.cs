bdea76463b122d4b32f3f7d64e52d516a31a0916
Shachi-17 <145852607+Shachi-17@users.noreply.github.com>
2025-09-02 16:28:05 +0800

    C# Programming Exercise 3: Loops

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,32 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+
+        Random rand = new Random();
+        int magic = rand.Next(1, 101);
+
+
+        Console.Write("What is your GUESS? ");
+        int guess = int.Parse(Console.ReadLine());
+
+
+        while (guess != magic)
+        {
+            if (guess < magic)
+            {
+                Console.WriteLine("HIGHER");
+            }
+            else
+            {
+                Console.WriteLine("LOWER");
+            }
+
+            Console.Write("What is your GUESS? ");
+            guess = int.Parse(Console.ReadLine());
+        }
+
+
+        Console.WriteLine("You guessed it!");
     }
 }
