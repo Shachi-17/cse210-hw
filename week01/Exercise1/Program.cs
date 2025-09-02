dc508ef38bee6b70833952b20a13ce1c7f84b857
Shachi-17 <145852607+Shachi-17@users.noreply.github.com>
2025-09-02 15:11:37 +0800

    C# Prep 01

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your first name? ");
+        string Fname = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string Lname = Console.ReadLine();
+        Console.Write($"Your name is {Lname}, {Fname} {Lname}.");
+
+        Console.ReadLine();
     }
 }
