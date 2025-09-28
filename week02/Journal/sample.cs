f492c88f907d1f14d090eb655502019a09862d34
Shachi-17 <145852607+Shachi-17@users.noreply.github.com>
2025-09-28 09:22:44 +0800

    commit

---
+++ week02/Journal/sample.cs
@@ -0,0 +1,21 @@
+public abstract class Shape
+{
+    public abstract double Area();
+}
+
+public class Rectangle : Shape
+{
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height)
+    {
+        this.width = width;
+        this.height = height;
+    }
+
+    public override double Area()
+    {
+        return width * height;
+    }
+}
--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -4,6 +4,52 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        Scriptures scriptures = new Scriptures();
+        var (reference, text) = scriptures.GetRandomScripture();
+
+        List<Word> words = text.Split(' ')
+            .Select(word => new Word(word))
+            .ToList();
+
+
+        while (true)
+        {
+            Console.Clear();
+
+            Console.WriteLine($"{reference.GetDisplayText()}");
+            Console.WriteLine(string.Join(" ", words.Select(w => w.GetDisplayText())));
+            Console.WriteLine("\nPress Enter to hide words or type 'quit' to exit.");
+
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "quit")
+                break;
+
+            HideRandomWords(words, 3);
+
+            if (words.All(w => w.IsHidden()))
+            {
+                Console.Clear();
+                Console.WriteLine($"{reference.GetDisplayText()}");
+                Console.WriteLine(string.Join(" ", words.Select(w => w.GetDisplayText())));
+                Console.WriteLine("\nAll words are hidden! Program finished.");
+                break;
+            }
+        }
+    }
+    static void HideRandomWords(List<Word> words, int count)
+    {
+        Random random = new Random();
+        var visibleWords = words.Where(w => !w.IsHidden()).ToList();
+
+
+        int toHide = Math.Min(count, visibleWords.Count);
+
+        for (int i = 0; i < toHide; i++)
+        {
+            int index = random.Next(visibleWords.Count);
+            visibleWords[index].Hide();
+            visibleWords.RemoveAt(index);
+        }
     }
 }
